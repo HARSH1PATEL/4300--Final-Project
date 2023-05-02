e2ae307c499d872682ab747fd0fb3b393005ca03
Ruchitha Rajaghatta <rsr262@cornell.edu>
2023-05-01 23:33:59 -0400

    hid api key

--- data_scraping/star_gen.py
+++ data_scraping/star_gen.py
@@ -7,6 +7,8 @@ with open('attrs.json', 'r') as f:
   data = json.load(f)
 f = open('stars9.txt', 'w')
 
+api_key = '' #taken out for security
+
 i = 0
 for place in data[2620:]:
   print(i)
@@ -15,27 +17,27 @@ for place in data[2620:]:
   search2 = urllib.parse.quote(place['attr_name'])
   search3 = urllib.parse.quote("tourist attractions in "+place["state_name"])
   try:
-    req = requests.get('https://maps.googleapis.com/maps/api/place/textsearch/json?query='+search1+'&key=AIzaSyAUZ11rhgywcPuyP1ICOF1UCcDDVmSnzKY', timeout=5)
+    req = requests.get('https://maps.googleapis.com/maps/api/place/textsearch/json?query='+search1+'&key='+api_key, timeout=5)
     time.sleep(1)
   except:
     try:
-      req = requests.get('https://maps.googleapis.com/maps/api/place/textsearch/json?query='+search2+'&key=AIzaSyAUZ11rhgywcPuyP1ICOF1UCcDDVmSnzKY', timeout=5)
+      req = requests.get('https://maps.googleapis.com/maps/api/place/textsearch/json?query='+search2+'&key='+api_key, timeout=5)
       time.sleep(1)
     except:
       try:
-        req = requests.get('https://maps.googleapis.com/maps/api/place/textsearch/json?query='+search3+'&key=AIzaSyAUZ11rhgywcPuyP1ICOF1UCcDDVmSnzKY', timeout=5)
+        req = requests.get('https://maps.googleapis.com/maps/api/place/textsearch/json?query='+search3+'&key='+api_key, timeout=5)
         time.sleep(1)
       except:
         f.write('3.0'+'\n')
 
   if req.json()['status'] == "ZERO_RESULTS":
     search = urllib.parse.quote(search2)
-    req = requests.get('https://maps.googleapis.com/maps/api/place/textsearch/json?query='+search2+'&key=AIzaSyAUZ11rhgywcPuyP1ICOF1UCcDDVmSnzKY', timeout=5)
+    req = requests.get('https://maps.googleapis.com/maps/api/place/textsearch/json?query='+search2+'&key='+api_key, timeout=5)
     time.sleep(1)
     print(req.status_code)
   if req.json()['status'] == "ZERO_RESULTS":
     search = urllib.parse.quote(search3)
-    req = requests.get('https://maps.googleapis.com/maps/api/place/textsearch/json?query='+search3+'&key=AIzaSyAUZ11rhgywcPuyP1ICOF1UCcDDVmSnzKY', timeout=5)
+    req = requests.get('https://maps.googleapis.com/maps/api/place/textsearch/json?query='+search3+'&key='+api_key, timeout=5)
     time.sleep(1)
     print(req.status_code)
   results = req.json()['results']
